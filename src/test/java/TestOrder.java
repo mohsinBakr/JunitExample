abd6c673ed1d0ed954a8d562aae1e6896406aa07
mohsinBakr <mohsin.bakr@gmail.com>
2022-11-26 14:01:36 +0200

    Update TestOrder.java

--- src/test/java/TestOrder.java
+++ src/test/java/TestOrder.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.*;
 
 public class TestOrder {
     Order order ;
+    OrderItem orderItem;
     double userBalance = 3.0;
     @Before
     public void setup(){
@@ -21,21 +22,48 @@ public class TestOrder {
         assertEquals(0,order.itemsCount());
     }
 
-    @Ignore
+    @Test
     public void testOrderAddItemIncreasesOrderItemsCountByOne(){
+        OrderItem Item1 = new OrderItem("Item1", 22);
+        order.addItem(Item1);
+        assertEquals(1,order.itemsCount());
+
     }
 
-    @Ignore
+    @Test
     public void testOrderAddItemIncreasesOrderTotalPriceByItemPrice(){
+        OrderItem Item1 = new OrderItem("Item1", 22);
+        OrderItem Item2 = new OrderItem("Item2", 44);
+        order.addItem(Item1);
+        order.addItem(Item2);
+        assertEquals(66.0,order.getTotalPrice());
 
     }
 
-    @Ignore
+    @Test
     public void testOrderRemoveItemFromOrder(){
+        OrderItem Item1 = new OrderItem("Item1", 22);
+        OrderItem Item2 = new OrderItem("Item2", 44);
+        OrderItem Item3 = new OrderItem("Item3", 66);
+        order.addItem(Item1);
+        order.addItem(Item2);
+        order.addItem(Item3);
+        assertEquals(3,order.itemsCount());
+        order.removeItem("Item1");
+        for (int i=0; i<order.itemsCount(); i++) {
+            System.out.println(order.getItems());
+            assertNotEquals("(Item1 - 22.000000)",order.getItems().get(i).toString());
+        }
+
+
+
 
     }
-    @Ignore
+    @Test
     public void testOrderProceedToCheckoutFailsWithInsufficientBalanceDoesntChangeOrderStatus() throws Exception {
-
+        OrderItem Item1 = new OrderItem("Item1", 22);
+        order.addItem(Item1);
+        assertEquals(1,order.itemsCount());
+        order.proceedToCheckout(10);
     }
 }
